57c61f2dfa12df30f75acabfdb8b4bf663e4040e
EranMaron <eran9maron@gmail.com>
2019-06-29 21:53:35 +0300

    with consts

---
+++ consts.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     DB_USER: process.env.DB_USER,
+//     DB_PASS: process.env.DB_PASS,
+//     MLAB_URL: process.env.MLAB_URL,
+// };
+
+module.exports = {
+    DB_USER: `db_usr`,
+    DB_PASS: `db_pass1`,
+    MLAB_URL: `mongodb://db_usr:db_pass1@ds263656.mlab.com:63656/givegetdb`,
+};
