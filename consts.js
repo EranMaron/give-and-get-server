1c693ecc29630733718831e2e3c020a3a3ed3674
EranMaron <eran9maron@gmail.com>
2019-07-01 20:35:09 +0300

    app server

--- consts.js
+++ consts.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DB_USER: `db_usr`,
-    DB_PASS: `db_pass1`,
-    MLAB_URL: `mongodb://db_usr:db_pass1@ds263656.mlab.com:63656/givegetdb`,
+    DB_USER: "db_usr",
+    DB_PASS: "db_pass1",
+    MLAB_URL: "mongodb://db_usr:db_pass1@ds263656.mlab.com:63656/givegetdb",
 };
--- controller/test.js
+++ controller/test.js
@@ -1,10 +1,10 @@
 const User = require("../models/user")
 
 
-exports.checkIfUserExist = (req, res) => {
+    exports.checkIfUserExist = (req, res) => {
         User.findOne({phone_number: req.body.user, password: req.body.pass})
         .then(data => {
-                if (data != null) {
+                if (data !== null) {
                     res.send({status: '200', user: data})
                 } else {
                     res.send({status: '400', message: "User was not found. Please check the phone number or the password."})
@@ -15,24 +15,35 @@ exports.checkIfUserExist = (req, res) => {
 
     exports.addNewTask = (req, res) => {
         User.findOne({phone_number: req.body.recieverPhoneNumber})
-            .then(data => data.updateOne({$push: {gotten_tasks: req.body}}))
-            .then(() => User.findOne({phone_number: req.body.userSendPhoneNumber}))
-            .then(data => data.updateOne({$push: {given_tasks: req.body}}))
-            .then(() => res.send({status: true, message: "Task Was Added Successfully"}))
-            .catch(err => res.send({status: '400', message: "User was not found. Please check the phone number or the password."}))
+            .then(data => {
+                if (data != null) {
+                    data.updateOne({$push: {gotten_tasks: req.body}})
+                    .then(() => User.findOne({phone_number: req.body.userSendPhoneNumber}))
+                    .then(data => data.updateOne({$push: {given_tasks: req.body}}))
+                    .then(() => res.send({status: true, message: "Task Was Added Successfully"}))   
+                } else {
+                    res.send({status: false, message: "User was not found. Please check the phone number."})
+                }
+            })
+            .catch(err => res.send({status: '400', message: "Error"}))            
     }
     
-    exports.addNewuser = (req, res) => {
-        console.log(req.body)
-        User.create({
-            phone_number: req.body.user,
-            password: req.body.pass,
-            name: req.body.name,
-            given_tasks: req.body.given_tasks,
-            gotten_task: req.body.gotten_task,
-            contact_list: req.body.contact_list
-        })
-            .then(() => res.send({status: true, user: req.body}))
-            .then(() => console.log("Added New User"))
-        .catch(err => console.log(err))
+exports.addNewuser = (req, res) => {
+    User.findOne({phone_number: req.body.user})
+        .then(data => {
+            if (data === null) {
+                User.create({
+                    phone_number: req.body.user,
+                    password: req.body.pass,
+                    name: req.body.name,
+                    given_tasks: req.body.given_tasks,
+                    gotten_task: req.body.gotten_task,
+                    contact_list: req.body.contact_list
+                })
+                .then(() => res.send({status: true, user: req.body}))   
+            } else {
+                res.send({status: false, message: "The user already exist!"})
+        }
+    })
+        .catch(err => res.send(err))
     }
