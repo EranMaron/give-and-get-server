fa763bfa90a0c6c7738d9c2ffb71fb64a68be62a
EranMaron <eran9maron@gmail.com>
2019-07-01 12:56:35 +0300

    Added new route

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     next();
     });        
 
-// app.get('/', (req, res) => res.send("Go"))
+app.get('/', (req, res) => res.send("Go"))
 app.post('/signin', handler.checkIfUserExist)
 app.post('/addTask', handler.addNewTask)
 app.post('/signup', handler.addNewuser)
