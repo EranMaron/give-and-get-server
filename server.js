5831d1892fa8af552557fd8f23263cf320552c65
EranMaron <eran9maron@gmail.com>
2019-07-01 15:23:50 +0300

    Added global route

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     next();
     });        
 
-app.get('/', (req, res) => res.send(process.env.DB_USER))
+app.get('/', (req, res) => res.send(process.env.DB_PASS))
 app.post('/signin', handler.checkIfUserExist)
 app.post('/addTask', handler.addNewTask)
 app.post('/signup', handler.addNewuser)
