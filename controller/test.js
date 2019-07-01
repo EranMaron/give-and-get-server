1528aacaf520d836b397d1c640d200c6e1294376
EranMaron <eran9maron@gmail.com>
2019-07-01 13:46:18 +0300

    Added global route

--- controller/test.js
+++ controller/test.js
@@ -2,7 +2,7 @@ const User = require("../models/user")
 
 
 exports.checkIfUserExist = (req, res) => {
-    res.send(`0${req.body.user}`)
+    res.send(`0 ${req.body.user}`)
 }
     // User.findOne({phone_number: user, password: req.body.pass})
     //     .then(data => {
